37631641d187965dcda76059f8abbd5a11680d6f
frontdevjs <front_dev_js@mail.ru>
2023-09-25 10:33:45 +0500

    initial project

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,5 +1,5 @@
 export const corsHeaders = {
-  'Access-Control-Allow-Origin': 'https://kazstroke.kz',
-  'Access-Control-Allow-Methods': 'GET, POST, HEAD, OPTIONS',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+    'Access-Control-Allow-Origin': 'https://kazstroke.kz',
+    'Access-Control-Allow-Methods': 'GET, POST, HEAD, OPTIONS',
+    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 }
--- supabase/functions/tg-bot/index.ts
+++ supabase/functions/tg-bot/index.ts
@@ -1,7 +1,11 @@
-import {serve} from "https://deno.land/std@0.168.0/http/server.ts"
-import {corsHeaders} from '../_shared/cors.ts'
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
 
-interface IFeedbackForm {
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+import {corsHeaders} from "../_shared/cors.ts";
+
+interface  IFeedbackForm {
   name: string;
   phone: string;
   email: string;
@@ -22,7 +26,10 @@ async function _sendToTelegram(feedback: IFeedbackForm) {
   });
 }
 
+
+
 serve(async (req) => {
+
   const { method } = req
 
   // This is needed if you're planning to invoke your function from a browser.
@@ -46,3 +53,9 @@ serve(async (req) => {
     })
   }
 })
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
